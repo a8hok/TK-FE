8c1851238a6cafdf950ff3ca03b6d99e0e1f4f85
aswin2062 <aswin.es2020eeel@sece.ac.in>
2023-06-04 01:21:04 +0530

    working on settings page

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Login from './Pages/Login/Login';
 import Resource from './Pages/Resource/Resource';
+import Settings from './Pages/Settings';
 const App: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/resource" element={<Resource />} />
+        <Route path="/settings" element={<Settings />} />
       </Routes>
     </BrowserRouter>
   );
