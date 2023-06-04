73f06d2bfa02f8289d1fbdc7fbc16b08516dc971
a8hok <ashokkumar.explore@gmail.com>
2023-06-04 08:34:05 +0530

    updated image to icons

--- src/Components/SurveyFooter/index.tsx
+++ src/Components/SurveyFooter/index.tsx
@@ -1,4 +1,5 @@
 import Logo from '../../Assets/PoweredBy.svg';
+
 function SurveyFooter() {
   return (
     <div className="bg-white fixed bottom-0 flex justify-end h-[85px] items-center w-[100vw]">
--- src/Pages/Dashboard/todo.tsx
+++ src/Pages/Dashboard/todo.tsx
@@ -1,13 +1,14 @@
-import TodoIcon from '../../assets/todo.svg';
+import { VscChecklist } from 'react-icons/vsc';
+
 function Todo() {
   return (
     <div
       className="bg-white rounded-xl shadow-lg w-fit p-[1%] background: #006AFF;
     items-center"
     >
-      <div className=" flex justify-between items-center">
+      <div className="flex gap-2 justify-between items-center">
+        <VscChecklist></VscChecklist>
         <div className="flex items-center">
-          <img src={TodoIcon} alt="" />
           <p className="text-[18px] text-[#34465D]">Things to do</p>
         </div>
         <p className="text-[14px] text-[#006AFF]">View All</p>
