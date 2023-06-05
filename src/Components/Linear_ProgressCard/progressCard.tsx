3ba168d337bca55653c090c525b3b476f387b0ba
kavin-x <kavinkumarab@gmail.com>
2023-06-05 17:20:43 +0530

    fix: minor design changes

--- src/Components/Linear_ProgressCard/progressCard.tsx
+++ src/Components/Linear_ProgressCard/progressCard.tsx
@@ -1,15 +1,17 @@
 import Banner from '../Assets/Rectangle1.svg';
-import 'react-sweet-progress/lib/style.css';
+
 const ProgressCard = () => {
   return (
     <div className="w-[200px] m-[2%] h-fit bg-learningcenter relative border rounded-2xl">
-      <img src={Banner} alt="" className="h-[250px] w-[200px] relative" />
+      <img src={Banner} alt="" className="h-fit w-[200px] relative" />
       <div className="p-[10%]">
         <div className=" absolute top-[50%] ">
           <p className="text-[18px] text-[#707683] w-fit ">
             Handshake connection
           </p>
-          <div className="flex justify-between w-full"></div>
+          <div className="bg-gray-200  h-2 dark:bg-gray-700 w-[90%] mt-5">
+            <div className="bg-blue-600 h-2 " style={{ width: '45%' }}></div>
+          </div>
         </div>
         <div className="flex justify-between  bottom-0 w-full">
           <p className="text-[10px] text-[#34465D]  "> Completed: 35%</p>
